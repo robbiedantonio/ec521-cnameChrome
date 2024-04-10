3f7b0219994e0fd6955707ac5641bcbea1926a56
PatKuz <pkuzdzal@bu.edu>
2024-04-10 11:18:12 -0400

    jsonify

--- cloakList/format_domains.py
+++ cloakList/format_domains.py
@@ -1,4 +1,4 @@
-import csv
+import csv, json
 
 def parseCsv():
     with open('cname_cloaking_site.csv', 'r') as f:
@@ -9,9 +9,28 @@ def parseCsv():
 def reformat(subdomain_list):
     with open('reformatted.txt', 'w') as f:
         for subdomain in subdomain_list:
-            f.write('{}:{}'.format(subdomain.split('.')[0], subdomain))
+            f.write('{}:{}\n'.format(subdomain.split('.')[0], subdomain))
 
+def toJSON():
+    # this is such awful code ahaha
+    with open('reformatted.txt', 'r') as f:
+        keys = []
+        values = []
+
+        for line in f:
+            line = line.strip().split(':')
+            keys.append(line[0])
+            values.append(line[1])
+        
+        dictionary = {key:[] for key in set(keys)}
+
+        for key, value in zip(keys, values):
+            dictionary[key].append(value)
+        
+    with open('subdomains.json', 'w') as f: 
+        json.dump(dictionary, f)
 
 if __name__ == '__main__':
     subdomain_list = parseCsv()
     reformat(subdomain_list)
+    toJSON()
